66686401bac97d88fa44c853e6bc8fc4ea0975dd
Đỗ Văn Tuấn <129986145+tuando18@users.noreply.github.com>
2023-05-22 20:03:30 +0700

    GD1

--- app/src/main/java/com/dovantuan/assignment1/SignupActivity.java
+++ app/src/main/java/com/dovantuan/assignment1/SignupActivity.java
@@ -1,14 +1,39 @@
 package com.dovantuan.assignment1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SignupActivity extends AppCompatActivity {
+    Button btnClick;
+    TextView btnLogin;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup_main);
+
+        intit();
+        addListener();
+    }
+
+    private void intit() {
+        btnClick = findViewById(R.id.btn_signup);
+        btnLogin = findViewById(R.id.txt_sign);
+    }
+
+    private void addListener() {
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
